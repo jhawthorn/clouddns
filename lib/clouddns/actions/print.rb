b6cf666a413277e4a88aebd79d8457eb2585da21
John Hawthorn <john.hawthorn@gmail.com>
2011-07-10 00:50:56 -0700

    missed puts

--- lib/clouddns/actions/print.rb
+++ lib/clouddns/actions/print.rb
@@ -2,7 +2,7 @@ module Clouddns
   module Actions
     class Print < GenericAction
       def run
-        "Zone '#{@zone.name}'"
+        puts "Zone '#{@zone.name}'"
         namelen = @zone.records.map{ |x| x.name.length }.max
         @zone.records.each do |record|
           record.value.each_with_index do |value, i|
