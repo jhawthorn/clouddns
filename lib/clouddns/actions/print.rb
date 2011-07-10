7c02e2816c7b67ea2683d26d0cd3e4ee9451fe50
John Hawthorn <john.hawthorn@gmail.com>
2011-07-10 00:46:34 -0700

    cleaup print for multiple values

--- lib/clouddns/actions/print.rb
+++ lib/clouddns/actions/print.rb
@@ -5,7 +5,14 @@ module Clouddns
         "Zone '#{@zone.name}'"
         namelen = @zone.records.map{ |x| x.name.length }.max
         @zone.records.each do |record|
-          puts "%5s %5s %#{namelen}s %s" % [record.type, record.ttl, record.name, record.value]
+          record.value.each_with_index do |value, i|
+            if i.zero?
+              args = [record.type, record.ttl, record.name, value]
+            else
+              args = ['', '', '', value]
+            end
+            puts "%5s %5s %#{namelen}s %s" % args
+          end
         end
       end
     end
